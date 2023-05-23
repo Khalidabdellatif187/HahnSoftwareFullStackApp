d96a0a7696c0ea349cf07c3b852ebb0d4f304e48
khalid187 <khalidabdellatif187@gmail.com>
2023-05-23 16:36:15 +0300

    Add Docker Compose to run application By It

--- database/employee-management.sql
+++ database/employee-management.sql
@@ -1,18 +1,17 @@
 create database Employee_Management;
 USE Employee_Management;
-CREATE table employee (
-id bigint primary KEY auto_increment , 
-first_name varchar(225) , 
-second_name varchar(225),
-email varchar (225),
-role varchar (225)
-);
+CREATE TABLE employee (
+       id bigint primary KEY auto_increment,
+       first_name varchar(225),
+       second_name varchar(225),
+       email varchar(225),
+       role varchar(225),
+       CONSTRAINT unique_name UNIQUE (first_name, second_name),
+       CONSTRAINT unique_email UNIQUE (email)
+	  );
+
 Select * from employee;
-Alter table employee add constraint unique_name Unique (first_name , second_name);
-Alter table employee add constraint unique_email Unique (email);
-select Max(employee.id) from employee;
-ALTER TABLE employee MODIFY id bigint NOT NULL;
-ALTER TABLE employee MODIFY id INT AUTO_INCREMENT NOT NULL;
+
 
 
 
