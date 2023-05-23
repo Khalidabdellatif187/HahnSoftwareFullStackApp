571569f00dac5b7875dbb99ae73978f2dc6a5854
khalid187 <khalidabdellatif187@gmail.com>
2023-05-23 18:03:15 +0300

    Change In Project Folders

---
+++ backend/EmployeeManagementSystemBackend/src/test/java/com/luv2code/EmployeeManagementSystemBackend/EmployeeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.EmployeeManagementSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
